115e1b1390ba14f57b523726ae574cfde65d191f
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-12-12 23:26:39 +0700

    separate server/client

--- client/global.d.ts
+++ client/global.d.ts
@@ -3,5 +3,5 @@ import { PrismaClient } from "@prisma/client";
 declare global {
   namespace globalThis {
     var prismadb: PrismaClient
-  };
-};
+  }
+}
--- client/lib/prismadb.ts
+++ client/lib/prismadb.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const client = global.prismadb || new PrismaClient();
 
+// prevent hot loading by storing in global
 if (process.env.NODE_ENV === "production") global.prismadb = client;
 
 export default client;
